71092a0a15bab75db6c9ff6d5b632f1a3e52b7ea
Andrew Mushel <almushel@protonmail.com>
2023-10-18 15:23:46 -0500

    Move board creation and logic into ChessBoard class

--- src/chess.py
+++ src/chess.py
@@ -17,100 +17,126 @@ class Team(IntEnum):
 	WHITE = 0
 	BLACK = auto()
 
+class ChessBoard:
+	BOARD_ROWS = 8
+	BOARD_COLS = 8
+
+	def __init__(self, grid_size, colors=(BLACK, WHITE)):
+		self.grid_size = grid_size
+		self.colors = colors
+
+		self.width = grid_size * self.BOARD_COLS
+		self.height = grid_size * self.BOARD_ROWS
+		
+		self.pieces = array("B", [0 for i in range(8 * 8)])
+		self.teams = array("B", [0 for i in range(8 * 8)])
+		self.center()
+		self.reset()
+		
+	def reset(self):
+		piece = Piece.NONE
+		for y in range(8):
+			for x in range(8):
+				if y == 1 or y == 6:
+					piece = Piece.PAWN
+				elif y == 0 or y == 7:
+					if x == 0 or x == 7:
+						piece = Piece.ROOK
+					if x == 1 or x == 6:
+						piece = Piece.KNIGHT
+					if x == 2 or x == 5:
+						piece = Piece.BISHOP
+					if x == 3:
+						if y == 0: piece = Piece.KING
+						else: piece = Piece.QUEEN
+					if x == 4:
+						if y == 0: piece = Piece.QUEEN
+						else: piece = Piece.KING
+				if piece != Piece.NONE:
+					index = y * 8 + x
+					if y < 4: self.teams[index] = Team.BLACK
+					self.pieces[index] = piece
+					piece = Piece.NONE
+	
+	def center(self):
+		self.x = (GetScreenWidth() - self.width) / 2
+		self.y = (GetScreenHeight() - self.height) / 2
+	
+	def draw_board(self):
+		piece_labels = ["" for i in range(Piece.QUEEN.value + 1)]
+		piece_labels[Piece.PAWN] 	= "P"
+		piece_labels[Piece.ROOK] 	= "R"
+		piece_labels[Piece.KNIGHT] 	= "N"
+		piece_labels[Piece.BISHOP] 	= "B"
+		piece_labels[Piece.KING] 	= "K"
+		piece_labels[Piece.QUEEN] 	= "Q"
+
+		draw_x = self.x
+		draw_y = self.y
+
+		DrawRectangleLinesEx(Rectangle(draw_x-2, draw_y-2, self.width+4, self.height+4), 4, BLACK)
+		
+		index = 0
+		square_color = 0
+		for y in range(8):
+			for x in range(8):
+
+				DrawRectangle(draw_x, draw_y, self.grid_size, self.grid_size, self.colors[square_color])
+				if self.pieces[index] != Piece.NONE:
+					team = self.teams[index]
+					piece = self.pieces[index]
+					padding = (self.grid_size - MeasureText(piece_labels[piece], self.grid_size)) / 2
+					p_color_1 = self.colors[int(not team)].rgba
+					p_color_2 = self.colors[square_color].rgba
+					p_color_2.a = 0
+					
+					DrawCircleGradient(draw_x + self.grid_size/2, draw_y + self.grid_size/2, self.grid_size/1.5, p_color_1, p_color_2)
+					DrawText(piece_labels[piece], padding + draw_x, draw_y, self.grid_size, self.colors[team])
+
+				draw_x += self.grid_size
+				index += 1
+				square_color = int(not square_color)
+			draw_x = self.x
+			draw_y += self.grid_size
+			square_color = int(not square_color)
+
+	def draw_labels(self):		
+		font_size = self.grid_size / 2
+		font_color = GRAY
+		offside_color = font_color.rgba
+		offside_color.a //= 2
+
+		padding_y = self.y + (self.grid_size - font_size) / 2
+		for i in range(8):
+			width = MeasureText("abcdefgh"[i], font_size)
+			padding_x = self.x + (self.grid_size - width) / 2
+
+			DrawText("abcdefgh"[i], padding_x + self.grid_size*i, padding_y + self.height, font_size, font_color)
+			DrawTextPro(GetFontDefault(), "abcdefgh"[i], Vector2(padding_x + self.grid_size*i, padding_y - self.grid_size), Vector2(width,font_size), 180, font_size, 0, offside_color)
+
+			width = MeasureText("12345678"[i], font_size)
+			padding_x = self.x + (self.grid_size - width) / 2
+			DrawText("12345678"[i], padding_x - self.grid_size, padding_y + self.grid_size*i, font_size, font_color)
+			DrawTextPro(GetFontDefault(), "12345678"[i], Vector2(padding_x + self.width, padding_y + self.grid_size*i), Vector2(width,font_size), 180, font_size, 0, offside_color)
+
+	def draw(self):
+		self.draw_board()
+		self.draw_labels()
+
 def main():
 	SCREEN_W = 800
 	SCREEN_H = 600
-	GRID_SIZE = 50	
+	GRID_SIZE = 50
 
 	InitWindow(SCREEN_W, SCREEN_H, "Net Chess")
-	
-	board_size = GRID_SIZE * 8
-	left = (SCREEN_W - board_size) / 2
-	top = (SCREEN_H - board_size) / 2
-	board_colors = (WHITE, BLACK)
 
-	piece_labels = ["" for i in range(Piece.QUEEN.value + 1)]
-	piece_labels[Piece.PAWN] 	= "P"
-	piece_labels[Piece.ROOK] 	= "R"
-	piece_labels[Piece.KNIGHT] 	= "N"
-	piece_labels[Piece.BISHOP] 	= "B"
-	piece_labels[Piece.KING] 	= "K"
-	piece_labels[Piece.QUEEN] 	= "Q"
-
-	board = {
-		"pieces": array("B", [0 for i in range(8 * 8)]),
-		"teams": array("B", [0 for i in range(8 * 8)])
-	}
-
-	piece = Piece.NONE
-	for y in range(8):
-		for x in range(8):
-			if y == 1 or y == 6:
-				piece = Piece.PAWN
-			elif y == 0 or y == 7:
-				if x == 0 or x == 7:
-					piece = Piece.ROOK
-				if x == 1 or x == 6:
-					piece = Piece.KNIGHT
-				if x == 2 or x == 5:
-					piece = Piece.BISHOP
-				if x == 3:
-					if y == 0: piece = Piece.KING
-					else: piece = Piece.QUEEN
-				if x == 4:
-					if y == 0: piece = Piece.QUEEN
-					else: piece = Piece.KING
-			if piece is not Piece.NONE:
-				index = y * 8 + x
-				if y < 4: board["teams"][index] = Team.BLACK
-				board["pieces"][index] = piece
-
-				piece = Piece.NONE
+	board = ChessBoard(GRID_SIZE)
 
 	while not WindowShouldClose():
 		BeginDrawing()
 		ClearBackground(RAYWHITE)
-		Color()
-		font_size = GRID_SIZE / 2
-		font_color = GRAY
-		offside_color = font_color.rgba
-		offside_color.a //= 2
-		padding_y = top + (GRID_SIZE - font_size) / 2
-		for i in range(8):
-			width = MeasureText("abcdefgh"[x], font_size)
-			padding_x = left + (GRID_SIZE - width) / 2
-
-			DrawText("abcdefgh"[i], padding_x + GRID_SIZE*i, padding_y + GRID_SIZE*8, font_size, font_color)
-			DrawTextPro(GetFontDefault(), "abcdefgh"[i], Vector2(padding_x + GRID_SIZE*i, padding_y - GRID_SIZE), Vector2(width,font_size), 180, font_size, 0, offside_color)
-
-			width = MeasureText("12345678"[x], font_size)
-			padding_x = left + (GRID_SIZE - width) / 2
-			DrawText("12345678"[i], padding_x - GRID_SIZE, padding_y + GRID_SIZE*i, font_size, font_color)
-			DrawTextPro(GetFontDefault(), "12345678"[i], Vector2(padding_x + GRID_SIZE*8, padding_y + GRID_SIZE*i), Vector2(width,font_size), 180, font_size, 0, offside_color)
-
-		DrawRectangleLinesEx(Rectangle(left-2, top-2, GRID_SIZE*8 + 4, GRID_SIZE*8 + 4), 4, BLACK)
-		square_color = 0
-		for y in range(8):
-			for x in range(8):
-				draw_x = left + x * GRID_SIZE
-				draw_y = top + y * GRID_SIZE
-				index = y * 8 + x
-
-				DrawRectangle(draw_x, draw_y, GRID_SIZE, GRID_SIZE, board_colors[square_color])
-				if board["pieces"][index] != Piece.NONE:
-					team = board["teams"][index]
-					piece = board["pieces"][index]
-					padding = (GRID_SIZE - MeasureText(piece_labels[piece], GRID_SIZE)) / 2
-					p_color_1 = board_colors[int(not team)].rgba
-					p_color_2 = board_colors[square_color].rgba
-					p_color_2.a = 0
-					
-					DrawCircleGradient(draw_x + GRID_SIZE/2, draw_y + GRID_SIZE/2, GRID_SIZE/1.5, p_color_1, p_color_2)
-#					DrawCircle(draw_x + GRID_SIZE/2, draw_y + GRID_SIZE/2, GRID_SIZE/2, board_colors[int(not team)])
-					DrawText(piece_labels[piece], padding + draw_x, draw_y, GRID_SIZE, board_colors[team])
-
-				square_color = int(not square_color)
-			square_color = int(not square_color)
+		
+		board.draw()
 
 		EndDrawing()
 
